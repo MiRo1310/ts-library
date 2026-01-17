4cc166bac393135820732c55bc13739ed25715f0
MiRo1310 <michael.roling@gmx.de>
2026-01-17 07:54:45 +0100

    Update CI configuration and enhance JSON validation tests

--- test/array/json.test.ts
+++ test/array/json.test.ts
@@ -21,24 +21,24 @@ describe("Json", () => {
     expect(toJsonString(1520)).toBe("1520");
   });
 
-  // test("jsonString to be inValid json", () => {
-  //   const result = toJSON("{'a': 1,'b': 'test','c': true}");
-  //   expect(result).to.have.property("isValidJson", false);
-  //   expect(result).to.have.property("json", null);
-  //   expect(typeof result.error).to.be.oneOf(["object", "string", "number", "boolean", "undefined"]);
-  // });
-  //
-  // test("jsonString to be valid json", () => {    
-  //   const result = toJSON("{\"a\": 1,\"b\": \"test\",\"c\": true}");
-  //   expect(result).to.have.property("isValidJson", true);
-  //   expect(result.error).toBeUndefined();
-  //   expect(result).to.have.property("json").that.deep.equal(obj);
-  // });
-  //
-  // test("jsonString to be valid json", () => {    
-  //   const result = toJSON(null);
-  //   expect(result).to.have.property("isValidJson", false);
-  //   expect(result).to.have.property("json", null);
-  //   expect(result).to.have.property("error", "Value is null");
-  // });
+  test("jsonString to be inValid json", () => {
+    const result = toJSON("{'a': 1,'b': 'test','c': true}");
+    expect(result).to.have.property("isValidJson", false);
+    expect(result).to.have.property("json", null);
+    expect(typeof result.error).to.be.oneOf(["object", "string", "number", "boolean", "undefined"]);
+  });
+
+  test("jsonString to be valid json", () => {    
+    const result = toJSON("{\"a\": 1,\"b\": \"test\",\"c\": true}");
+    expect(result).to.have.property("isValidJson", true);
+    expect(result.error).toBeUndefined();
+    expect(result).to.have.property("json").that.deep.equal(obj);
+  });
+
+  test("jsonString to be valid json", () => {    
+    const result = toJSON(null);
+    expect(result).to.have.property("isValidJson", false);
+    expect(result).to.have.property("json", null);
+    expect(result).to.have.property("error", "Value is null");
+  });
 });
