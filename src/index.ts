005fcd0fac2e2c3a305d9b45e466ff4d6d90cee4
MiRo1310 <michael.roling@gmx.de>
2026-01-18 09:43:14 +0100

    Enhance package.json exports and update TypeScript configuration for declaration files

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ import "./prototype/string";
 import "./prototype/number";
 
 export { toJsonString, toJSON } from "./json";
-export {deepCopy, isFalsy, isDefined, isTruthy,isString } from "./lib";
+export { deepCopy, isFalsy, isDefined, isTruthy, isString } from "./lib";
