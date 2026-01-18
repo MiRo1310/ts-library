0f50a06f4397034031abb485584cc89d4a44ae91
MiRo1310 <68854794+MiRo1310@users.noreply.github.com>
2026-01-18 15:35:04 +0100

    Enhance package.json exports and update TypeScript configuration for declaration files (#3)
    * Enhance package.json exports and update TypeScript configuration for declaration files
    
    * Refactor type definitions and update function exports in index.ts and lib.d.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,10 +1,12 @@
-export const isTruthy = (value: any): boolean => isDefined(value) && ["1", 1, true, "true"].includes(value);
+import { DeepCopy, IsDefined, IsFalsy, IsString, IsTruthy } from "../types/lib";
 
-export const isFalsy = (value: any): boolean => ["0", 0, false, "false", undefined, null].includes(value);
+export const isTruthy: IsTruthy = (value: any): boolean => isDefined(value) && ["1", 1, true, "true"].includes(value);
 
-export const isDefined = <T>(value: T | undefined | null): value is T => value !== undefined && value !== null;
+export const isFalsy: IsFalsy = (value: any): boolean => ["0", 0, false, "false", undefined, null].includes(value);
 
-export const deepCopy = <T>(value: T, errorHandler?: (e: any) => void): T | undefined => {
+export const isDefined: IsDefined = <T>(value: T | undefined | null): value is T => value !== undefined && value !== null;
+
+export const deepCopy: DeepCopy = <T>(value: T, errorHandler?: (e: any) => void): T | undefined => {
   try {
     return !isDefined(value) ? undefined : JSON.parse(JSON.stringify(value));
   } catch (err) {
@@ -17,4 +19,4 @@ export const deepCopy = <T>(value: T, errorHandler?: (e: any) => void): T | unde
   }
 };
 
-export const isString = (value: unknown): value is string => typeof value === "string";
+export const isString: IsString = (value: unknown): value is string => typeof value === "string";
